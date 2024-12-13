c0138147d70f919731e36f7c7924718a99d871d8
igor <vons.138756@e.unavarra.es>
2024-12-13 17:07:05 +0100

    Implemented basic value adjustemnt for height and width of the chip, coin diameter and thickness

--- main.py
+++ main.py
@@ -23,6 +23,8 @@ from models import Geometries, def_dimensions
 class CadQueryViewer(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.geometries = Geometries()
+        self.custom_dimensions = def_dimensions
         self.setWindowTitle("CadQuery 3D Viewer")
         self.setGeometry(100, 100, 800, 800)
 
@@ -86,28 +88,149 @@ class CadQueryViewer(QMainWindow):
         picker_layout = QVBoxLayout(picker_widget)
         picker_layout.setAlignment(Qt.AlignTop)  # Align the layout to the top
     
-        # Example slider for adjusting a parameter
-        slider_label = QLabel("Adjust Parameter:")
-        picker_layout.addWidget(slider_label)
+        # Slider for adjusting coin diameter
+        slider_label_cd = QLabel("Adjust Coin Diameter:")
+        picker_layout.addWidget(slider_label_cd)
     
-        slider_layout = QHBoxLayout()
-        slider = QSlider(Qt.Horizontal)
-        slider.setMinimum(10)
-        slider.setMaximum(1000)
-        slider.setValue(500)
-        slider.valueChanged.connect(self.on_slider_value_changed)
-        slider_layout.addWidget(slider)
+        slider_layout_cd = QHBoxLayout()
+        
+        # Minimum value label for coin diameter
+        min_cd = self.geometries.min_cd
+        max_cd = self.geometries.max_cd
+        cd = self.geometries.cd
+
+        min_label_cd = QLabel(str(min_cd))
+        slider_layout_cd.addWidget(min_label_cd)
     
-        slider_value = QDoubleSpinBox()
-        slider_value.setDecimals(1)
-        slider_value.setSingleStep(0.1)
-        slider_value.setRange(1.0, 100.0)
-        slider_value.setValue(slider.value() / 10.0)
-        slider_value.valueChanged.connect(lambda value: slider.setValue(int(value * 10)))
-        slider.valueChanged.connect(lambda value: slider_value.setValue(value / 10.0))
-        slider_layout.addWidget(slider_value)
+        slider_cd = QSlider(Qt.Horizontal)
+        slider_cd.setMinimum(min_cd * 10)  # Scale by 10 to allow one decimal place
+        slider_cd.setMaximum(max_cd * 10)
+        slider_cd.setValue(cd * 10)
+        slider_cd.valueChanged.connect(self.on_slider_value_changed_cd)
+        slider_layout_cd.addWidget(slider_cd)
     
-        picker_layout.addLayout(slider_layout)
+        # Maximum value label for coin diameter
+        max_label_cd = QLabel(str(max_cd))
+        slider_layout_cd.addWidget(max_label_cd)
+    
+        slider_value_cd = QDoubleSpinBox()
+        slider_value_cd.setDecimals(1)
+        slider_value_cd.setSingleStep(0.1)
+        slider_value_cd.setRange(min_cd, max_cd)
+        slider_value_cd.setValue(slider_cd.value() / 10.0)
+        slider_value_cd.valueChanged.connect(lambda value: slider_cd.setValue(int(value * 10)))
+        slider_cd.valueChanged.connect(lambda value: slider_value_cd.setValue(value / 10.0))
+        slider_layout_cd.addWidget(slider_value_cd)
+    
+        picker_layout.addLayout(slider_layout_cd)
+    
+        # Slider for adjusting coin thickness
+        slider_label_ct = QLabel("Adjust Coin Thickness:")
+        picker_layout.addWidget(slider_label_ct)
+    
+        slider_layout_ct = QHBoxLayout()
+        
+        # Minimum value label for coin thickness
+        min_ct = self.geometries.min_ct
+        max_ct = self.geometries.max_ct
+        ct = self.geometries.ct
+
+        min_label_ct = QLabel(str(min_ct))
+        slider_layout_ct.addWidget(min_label_ct)
+    
+        slider_ct = QSlider(Qt.Horizontal)
+        slider_ct.setMinimum(min_ct * 10)
+        slider_ct.setMaximum(max_ct * 10)
+        slider_ct.setValue(ct * 10)
+        slider_ct.valueChanged.connect(self.on_slider_value_changed_ct)
+        slider_layout_ct.addWidget(slider_ct)
+    
+        # Maximum value label for coin thickness
+        max_label_ct = QLabel(str(max_ct))
+        slider_layout_ct.addWidget(max_label_ct)
+    
+        slider_value_ct = QDoubleSpinBox()
+        slider_value_ct.setDecimals(1)
+        slider_value_ct.setSingleStep(0.1)
+        slider_value_ct.setRange(min_ct, max_ct)
+        slider_value_ct.setValue(slider_ct.value() / 10.0)
+        slider_value_ct.valueChanged.connect(lambda value: slider_ct.setValue(int(value * 10)))
+        slider_ct.valueChanged.connect(lambda value: slider_value_ct.setValue(value / 10.0))
+        slider_layout_ct.addWidget(slider_value_ct)
+    
+        picker_layout.addLayout(slider_layout_ct)
+    
+        # Slider for adjusting chip width
+        slider_label_w = QLabel("Adjust Chip Width:")
+        picker_layout.addWidget(slider_label_w)
+    
+        slider_layout_w = QHBoxLayout()
+        
+        # Minimum value label for chip width
+        min_w = 1.0
+        max_w = 70.0
+        w = self.geometries.w
+
+        min_label_w = QLabel(str(min_w))
+        slider_layout_w.addWidget(min_label_w)
+    
+        slider_w = QSlider(Qt.Horizontal)
+        slider_w.setMinimum(min_w * 10)
+        slider_w.setMaximum(max_w * 10)
+        slider_w.setValue(w * 10)
+        slider_w.valueChanged.connect(self.on_slider_value_changed_w)
+        slider_layout_w.addWidget(slider_w)
+    
+        # Maximum value label for chip width
+        max_label_w = QLabel(str(max_w))
+        slider_layout_w.addWidget(max_label_w)
+    
+        slider_value_w = QDoubleSpinBox()
+        slider_value_w.setDecimals(1)
+        slider_value_w.setSingleStep(0.1)
+        slider_value_w.setRange(min_w, max_w)
+        slider_value_w.setValue(slider_w.value() / 10.0)
+        slider_value_w.valueChanged.connect(lambda value: slider_w.setValue(int(value * 10)))
+        slider_w.valueChanged.connect(lambda value: slider_value_w.setValue(value / 10.0))
+        slider_layout_w.addWidget(slider_value_w)
+    
+        picker_layout.addLayout(slider_layout_w)
+    
+        # Slider for adjusting chip height
+        slider_label_h = QLabel("Adjust Chip Height:")
+        picker_layout.addWidget(slider_label_h)
+    
+        slider_layout_h = QHBoxLayout()
+        
+        # Minimum value label for chip height
+        min_h = 1.0
+        max_h = 8.0
+        h = self.geometries.h
+
+        min_label_h = QLabel(str(min_h))
+        slider_layout_h.addWidget(min_label_h)
+    
+        slider_h = QSlider(Qt.Horizontal)
+        slider_h.setMinimum(min_h * 10)
+        slider_h.setMaximum(max_h * 10)
+        slider_h.setValue(h * 10)
+        slider_h.valueChanged.connect(self.on_slider_value_changed_h)
+        slider_layout_h.addWidget(slider_h)
+    
+        # Maximum value label for chip height
+        max_label_h = QLabel(str(max_h))
+        slider_layout_h.addWidget(max_label_h)
+    
+        slider_value_h = QDoubleSpinBox()
+        slider_value_h.setDecimals(1)
+        slider_value_h.setSingleStep(0.1)
+        slider_value_h.setRange(min_h, max_h)
+        slider_value_h.setValue(slider_h.value() / 10.0)
+        slider_value_h.valueChanged.connect(lambda value: slider_h.setValue(int(value * 10)))
+        slider_h.valueChanged.connect(lambda value: slider_value_h.setValue(value / 10.0))
+        slider_layout_h.addWidget(slider_value_h)
+    
+        picker_layout.addLayout(slider_layout_h)
     
         # Example dropdown for selecting a parameter
         dropdown_label = QLabel("Select Option:")
@@ -124,56 +247,24 @@ class CadQueryViewer(QMainWindow):
         picker_layout.addWidget(apply_button)
     
         return picker_widget
-        """
-        Create the picker section for modifying values.
-        """
-        picker_widget = QWidget()
-        picker_layout = QVBoxLayout(picker_widget)
-        picker_layout.setAlignment(Qt.AlignTop)  # Align the layout to the top
-
-        # Example slider for adjusting a parameter
-        slider_label = QLabel("Adjust Parameter:")
-        picker_layout.addWidget(slider_label)
-
-        slider = QSlider(Qt.Horizontal)
-        slider.setMinimum(1)
-        slider.setMaximum(100)
-        slider.setValue(50)
-        slider.valueChanged.connect(self.on_slider_value_changed)
-        picker_layout.addWidget(slider)
-
-        # Example dropdown for selecting a parameter
-        dropdown_label = QLabel("Select Option:")
-        picker_layout.addWidget(dropdown_label)
-
-        dropdown = QComboBox()
-        dropdown.addItems(["Option 1", "Option 2", "Option 3"])
-        dropdown.currentIndexChanged.connect(self.on_dropdown_changed)
-        picker_layout.addWidget(dropdown)
-
-        # Example button to apply changes
-        apply_button = QPushButton("Apply Changes")
-        apply_button.clicked.connect(self.on_apply_changes)
-        picker_layout.addWidget(apply_button)
-
-        return picker_widget
-
+    
+    
     def render_models(self):
         """
         Render the central and external pieces in their respective viewports.
         """
-        custom_dimensions = def_dimensions
-        custom_dimensions["cd"] = 26
-        #custom_dimensions["ct"] = 3
-        # Create an instance of the Geometries class
-        geom = Geometries(dimensions=custom_dimensions)
+        # Iitialize all the sliders to the default values
+        self.slider_value_cd = self.geometries.cd
+        self.slider_value_ct = self.geometries.ct
+        self.slider_value_w = self.geometries.w
+        self.slider_value_h = self.geometries.h
 
         # Central piece in the first viewport
-        central_piece = geom.central_piece()
+        central_piece = self.geometries.central_piece()
         self.add_model_to_renderer(self.viewport1["vtk_renderer"], central_piece)
 
         # External piece in the second viewport
-        external_piece = geom.external_piece()
+        external_piece = self.geometries.external_piece()
         self.add_model_to_renderer(self.viewport2["vtk_renderer"], external_piece)
 
     def add_model_to_renderer(self, renderer, model):
@@ -230,14 +321,41 @@ class CadQueryViewer(QMainWindow):
         event.accept()
 
     # Picker section event handlers
-    def on_slider_value_changed(self, value):
-        print(f"Slider value changed: {value}")
+    def on_slider_value_changed_cd(self, value):
+        self.slider_value_cd = (value / 10.0)
+    
+    def on_slider_value_changed_ct(self, value):
+        self.slider_value_ct = (value / 10.0)
+    
+    def on_slider_value_changed_w(self, value):
+        self.slider_value_w = (value / 10.0)
+    
+    def on_slider_value_changed_h(self, value):
+        self.slider_value_h = (value / 10.0)
 
     def on_dropdown_changed(self, index):
         print(f"Dropdown selection changed: {index}")
 
     def on_apply_changes(self):
-        print("Apply changes button clicked!")
+
+        self.geometries.cd = self.slider_value_cd
+        self.geometries.ct = self.slider_value_ct
+        self.geometries.w = self.slider_value_w
+        self.geometries.h = self.slider_value_h
+ 
+        # Clear the existing actors from the renderer
+        self.viewport1["vtk_renderer"].RemoveAllViewProps()
+    
+        # Render the updated central piece
+        central_piece = self.geometries.central_piece()
+        self.add_model_to_renderer(self.viewport1["vtk_renderer"], central_piece)
+
+        # Clear the existing actors from the renderer
+        self.viewport2["vtk_renderer"].RemoveAllViewProps()
+
+        # Render the updated external piece
+        external_piece = self.geometries.external_piece()
+        self.add_model_to_renderer(self.viewport2["vtk_renderer"], external_piece)
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -2,101 +2,165 @@ import cadquery as cq
 import numpy as np
 
 def_dimensions = {
-        "w": 50,     # Width
-        "iw": 38,    # Inner width
-        "h": 6,      # Height
-        "cd": 46,    # Coin diameter
-        "ct": 1.5,   # Coin thickness
-        "pdd": 4.6,  # Pin diagonal distance
-        "phd": 1.5,  # Pin hole diameter
-        "cf": 0.75,  # Coin fillet
-        "pc": 0.5,   # Pin chamfer
-        "pbd": 1.6,  # Pin base diameter
-        "pd": 1.4,   # Pin diameter
-        "phhd": 1.45 # Pin head hole diameter
+        "chip width": 50,               # Width
+        "chip window width": 38,        # Inner width
+        "chip height": 6,               # Height
+        "coin diameter": 46,            # Coin diameter
+        "coin thickness": 1.5,          # Coin thickness
+        "pin diagonal distance": 4.6,   # Pin diagonal distance
+        "pin hole diameter": 1.5,       # Pin hole diameter
+        "coin fillet": 0.75,            # Coin fillet
+        "pin chamfer": 0.5,             # Pin chamfer
+        "pin base diameter": 1.6,       # Pin base diameter
+        "pin diameter": 1.4,            # Pin diameter
+        "pin head hole diameter": 1.45  # Pin head hole diameter
     }
 
 class Geometries:
+    """
+    Class to define the geometries of the pieces of the chip that is to hold the coin.
+    """
+
+
     def __init__(self, dimensions=def_dimensions):
-        self.w = dimensions["w"]       # Width
-        self.hw = self.w / 2           # Half width
-        self.iw = dimensions["iw"]     # Inner width
-        self.h = dimensions["h"]       # Height
-        self.cd = dimensions["cd"]     # Coin diameter
-        self.ct = dimensions["ct"]     # Coin thickness
-        self.pdd = dimensions["pdd"]   # Pin diagonal distance
-        self.phd = dimensions["phd"]   # Pin hole diameter
-        self._cf = dimensions["cf"]     # Coin fillet
-        self._pc = dimensions["pc"]     # Pin chamfer
-        self.pbd = dimensions["pbd"]   # Pin base diameter
-        self.pd = dimensions["pd"]     # Pin diameter
-        self.phhd = dimensions["phhd"] # Pin head hole diameter
+        self.w = dimensions["chip width"]                   # Width
+        """The width of the chip"""
+
+        self.iw = dimensions["chip window width"]           # Inner width
+        """The inner width reserverd for the window of the chip"""
+
+        self.h = dimensions["chip height"]                  # Height
+        """The height (total thickness) of the chip"""
+
+        self.cd = dimensions["coin diameter"]               # Coin diameter
+        """The diameter of the coin"""
+
+        self.ct = dimensions["coin thickness"]              # Coin thickness
+        """The thickness of the coin"""
+        
+        self._pdd = dimensions["pin diagonal distance"]      # Pin diagonal distance
+        """The distance between the corner of the chip and the center of the pin"""
+
+        self._phd = dimensions["pin hole diameter"]          # Pin hole diameter
+        """The diameter of the hole for the pin"""
+
+        self._cf = dimensions["coin fillet"]                # Coin fillet
+        """The radius of the fillet of the coin"""
+
+        self._pc = dimensions["pin chamfer"]                # Pin chamfer
+        """The distance of the chamfer of the pin"""
+
+        self._pbd = dimensions["pin base diameter"]          # Pin base diameter
+        """The diameter of the base of the pin"""
+
+        self._pd = dimensions["pin diameter"]                # Pin diameter
+        """The diameter of the pin"""
+
+        self._phhd = dimensions["pin head hole diameter"]    # Pin head hole diameter
+        """The diameter of the hole for the pin head"""
 
     @property
-    def cr(self):
-        return self.cd / 2  # Coin radius
+    def _cr(self):
+        """Coin radius"""
+        return self.cd / 2
 
     @property
-    def pld(self):
-        return np.sqrt((self.pdd**2) / 2)  # Pin linear distance
+    def _pld(self):
+        """Pin linear distance"""
+        return np.sqrt((self._pdd**2) / 2)
+    
+    @property
+    def _hw(self):
+        """Half width of the chip"""
+        return self.w / 2
 
     @property
-    def ppsw(self):
-        return self.w - 2 * self.pld  # Pin position square width
-    
-    @property
-    def first_diagonal(self):
-        return [(self.hw - self.pld, self.hw - self.pld), (-self.hw + self.pld, -self.hw + self.pld)]
-    
-    @property
-    def second_diagonal(self):
-        return [(self.hw - self.pld, -self.hw + self.pld), (-self.hw + self.pld, self.hw - self.pld)]
+    def _ppsw(self):
+        """Pin position square width"""
+        return self.w - 2 * self._pld
 
     @property
-    def mch(self):
-        return self.ct # Mid chip height
-    
-    @property
-    def cf(self):
-        return min(self._cf, self.mch/2-.001) # Coin fillet (.001 correction to avoid errors)
-    
-    @property
-    def pc(self):
-        return min(self._pc, self.mch/2-.001) # Pin chamfer (.001 correction to avoid errors)
+    def _first_diagonal(self):
+        """First diagonal coordinates"""
+        return [(self._hw - self._pld, self._hw - self._pld), (-self._hw + self._pld, -self._hw + self._pld)]
 
     @property
-    def ech(self):
-        return (self.h - self.ct)/2 # External chip height
+    def _second_diagonal(self):
+        """Second diagonal coordinates"""
+        return [(self._hw - self._pld, -self._hw + self._pld), (-self._hw + self._pld, self._hw - self._pld)]
+
+    @property
+    def _mch(self):
+        """Mid chip height"""
+        return self.ct
+
+    @property
+    def _ccf(self):
+        """Coin fillet with correction to avoid errors"""
+        return min(self._cf, self._mch / 2 - .001)
+
+    @property
+    def _cpc(self):
+        """Pin chamfer with correction to avoid errors"""
+        return min(self._pc, self._mch / 2 - .001)
+
+    @property
+    def _ech(self):
+        """External chip height"""
+        return (self.h - self.ct) / 2
+
+    @property
+    def _ph(self):
+        """Pin height"""
+        return self._mch + self._ech + .718
+
+    @property
+    def _pbh(self):
+        """Pin base height"""
+        return self._ech + .875
+
+    @property
+    def _phhh(self):
+        """Pin head hole height"""
+        return self._ech - .73
+        
+    @property
+    def max_cd(self):
+        """Maximum diameter of the coin"""
+        return (self.w - 2*self._ccf) - .1
     
     @property
-    def ph(self):
-        return self.mch + self.ech + .718  # Pin height
+    def max_ct(self):
+        """Maximum thickness of the coin"""
+        return min((self.h - 2*self._phhh) - .1, 3.5) # Didn't find any coin thicker than 3.5mm thus this is the maximum thickness
     
     @property
-    def pbh(self):
-        return self.ech + .875 # Pin base height
+    def min_cd(self):
+        """Minimum diameter of the coin"""
+        return 10 # Honestly, I don't know which should be the minimum diameter of a coin
     
     @property
-    def phhh(self):
-        return self.ech - .73 # Pin head hole height
-    
+    def min_ct(self):
+        """Minimum thickness of the coin"""
+        return 1.0 # The 3D model will breack if the thickness is less than 1.0 (I didn't find any coins with thickness less than 1.0 regardless)
+
     def central_piece(self):
 
         # Start with the base workplane, creating outer and inner rectangles
         result = cq.Workplane("XY")
         result = result.moveTo(0, 0).rect(self.w, self.w)  # Outer rectangle
-        result = result.rect(self.ppsw, self.ppsw, forConstruction=True)  # Inner construction rectangle
+        result = result.rect(self._ppsw, self._ppsw, forConstruction=True)  # Inner construction rectangle
 
         # Add circles at each vertex of the inner construction rectangle
-        result = result.vertices().circle(self.phd).end(2)
+        result = result.vertices().circle(self._phd).end(2)
 
         # Add a larger circle in the center
-        result = result.circle(self.cr).tag("base")  # Tagging for later reference
+        result = result.circle(self._cr).tag("base")  # Tagging for later reference
 
         # Extrude the base face and apply chamfers and fillets
-        result = result.faces().tag("base").extrude(self.mch)
-        result = result.edges(cq.selectors.RadiusNthSelector(0)).chamfer(self.pc)
-        result = result.edges(cq.selectors.RadiusNthSelector(2)).fillet(self.cf)
+        result = result.faces().tag("base").extrude(self._mch)
+        result = result.edges(cq.selectors.RadiusNthSelector(0)).chamfer(self._cpc)
+        result = result.edges(cq.selectors.RadiusNthSelector(2)).fillet(self._ccf)
 
         # Final result
         return result
@@ -107,73 +171,73 @@ class Geometries:
         result = result.moveTo(0, 0).rect(self.w, self.w)
 
         # Place circles in two specific positions and tag for later reference
-        result = result.pushPoints(self.first_diagonal)
-        result = result.circle(self.pbd).end(1)
-        result = result.pushPoints(self.second_diagonal)
-        result = result.circle(self.phhd).end(1)
-        result = result.faces().extrude(self.ech)
+        result = result.pushPoints(self._first_diagonal)
+        result = result.circle(self._pbd).end(1)
+        result = result.pushPoints(self._second_diagonal)
+        result = result.circle(self._phhd).end(1)
+        result = result.faces().extrude(self._ech)
 
 
-        result = result.pushPoints(self.first_diagonal)
-        result = result.circle(self.pbd)
-        result = result.faces().extrude(self.pbh)
+        result = result.pushPoints(self._first_diagonal)
+        result = result.circle(self._pbd)
+        result = result.faces().extrude(self._pbh)
 
-        result = result.pushPoints(self.first_diagonal)
-        result = result.circle(self.pd)
-        result = result.faces().extrude(self.ph)
+        result = result.pushPoints(self._first_diagonal)
+        result = result.circle(self._pd)
+        result = result.faces().extrude(self._ph)
 
-        result = result.pushPoints(self.second_diagonal)
-        result = result.circle(self.phhd)
-        result = result.faces().extrude(self.phhh)
+        result = result.pushPoints(self._second_diagonal)
+        result = result.circle(self._phhd)
+        result = result.faces().extrude(self._phhh)
 
 
         # Create a triangle on a new workplane
 
         triangle = cq.Workplane("YZ")
-        triangle = triangle.workplane(offset=(self.first_diagonal[0][0]))
-        triangle = triangle.center(self.first_diagonal[0][1] + self.pd, self.ph)
+        triangle = triangle.workplane(offset=(self._first_diagonal[0][0]))
+        triangle = triangle.center(self._first_diagonal[0][1] + self._pd, self._ph)
         triangle = triangle.polyline([(0, 0), (0.4, -0.359), (0, -0.718), (0, 0)])  # Define a triangle
         triangle = triangle.close()
-        triangle = triangle.sweep(result.moveTo(*self.first_diagonal[0]).circle(self.pd))	
+        triangle = triangle.sweep(result.moveTo(*self._first_diagonal[0]).circle(self._pd))	
 
         triangle1 = cq.Workplane("YZ")
-        triangle1 = triangle1.workplane(offset=(self.first_diagonal[1][0]))
-        triangle1 = triangle1.center(self.first_diagonal[1][1] - self.pd, self.ph)
+        triangle1 = triangle1.workplane(offset=(self._first_diagonal[1][0]))
+        triangle1 = triangle1.center(self._first_diagonal[1][1] - self._pd, self._ph)
         triangle1 = triangle1.polyline([(0, 0), (-0.4, -0.359), (0, -0.718), (0, 0)])  # Define a triangle
         triangle1 = triangle1.close()
-        triangle1 = triangle1.sweep(result.moveTo(*self.first_diagonal[1]).circle(self.pd))	
+        triangle1 = triangle1.sweep(result.moveTo(*self._first_diagonal[1]).circle(self._pd))	
 
 
         # Use the sweep operation
         result = result.union(triangle).union(triangle1)
 
-        result = result.edges(cq.selectors.BoxSelector((-self.hw,-self.hw,self.ph-.72),(self.hw,self.hw,self.ph+.1))).fillet(0.26)
+        result = result.edges(cq.selectors.BoxSelector((-self._hw,-self._hw,self._ph-.72),(self._hw,self._hw,self._ph+.1))).fillet(0.26)
 
-        for point in self.first_diagonal:
-            hole =  cq.Workplane("XY").workplane(offset=(self.ph)).center(*point).circle(0.75).rect(0.4,5).extrude(-1).rotateAboutCenter((0, 0, 10), -45)
+        for point in self._first_diagonal:
+            hole =  cq.Workplane("XY").workplane(offset=(self._ph)).center(*point).circle(0.75).rect(0.4,5).extrude(-1).rotateAboutCenter((0, 0, 10), -45)
 
             result = result.cut(hole)
 
         triangle = cq.Workplane("YZ")
-        triangle = triangle.workplane(offset=(self.second_diagonal[0][0]))
-        triangle = triangle.center(self.second_diagonal[0][1] - self.phhd, self.ech)
+        triangle = triangle.workplane(offset=(self._second_diagonal[0][0]))
+        triangle = triangle.center(self._second_diagonal[0][1] - self._phhd, self._ech)
         triangle = triangle.polyline([(0, 0), (-0.4, -0.365), (0, -0.73), (0, 0)])  # Define a triangle
         triangle = triangle.close()
-        triangle = triangle.sweep(result.moveTo(*self.second_diagonal[0]).circle(self.phhd))	
+        triangle = triangle.sweep(result.moveTo(*self._second_diagonal[0]).circle(self._phhd))	
 
         triangle1 = cq.Workplane("YZ")
-        triangle1 = triangle1.workplane(offset=(self.second_diagonal[1][0]))
-        triangle1 = triangle1.center(self.second_diagonal[1][1] + self.phhd, self.ech)
+        triangle1 = triangle1.workplane(offset=(self._second_diagonal[1][0]))
+        triangle1 = triangle1.center(self._second_diagonal[1][1] + self._phhd, self._ech)
         triangle1 = triangle1.polyline([(0, 0), (0.4, -0.365), (0, -0.73), (0, 0)])  # Define a triangle
         triangle1 = triangle1.close()
-        triangle1 = triangle1.sweep(result.moveTo(*self.second_diagonal[1]).circle(self.phhd))	
+        triangle1 = triangle1.sweep(result.moveTo(*self._second_diagonal[1]).circle(self._phhd))	
 
         result = result.cut(triangle).cut(triangle1)
-        result = result.edges(cq.selectors.BoxSelector((-self.hw+.1,self.hw-.1,self.ech+.1),(0,0,0))).fillet(0.123)
-        result = result.edges(cq.selectors.BoxSelector((self.hw-.1,-self.hw+.1,self.ech+.1),(0,0,0))).fillet(0.123)
+        result = result.edges(cq.selectors.BoxSelector((-self._hw+.1,self._hw-.1,self._ech+.1),(0,0,0))).fillet(0.123)
+        result = result.edges(cq.selectors.BoxSelector((self._hw-.1,-self._hw+.1,self._ech+.1),(0,0,0))).fillet(0.123)
 
 
-        result = result.cut(cq.Workplane("XY").rect(self.iw, self.iw).extrude(self.ech))
+        result = result.cut(cq.Workplane("XY").rect(self.iw, self.iw).extrude(self._ech))
 
         
         return result
